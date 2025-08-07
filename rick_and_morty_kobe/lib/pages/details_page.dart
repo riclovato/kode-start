fc2287fdac72f0eea1f320657542215311385c37
riclovato <riic.lovato@gmail.com>
2025-08-07 17:58:37 -0300

    detailsPage route fixed

--- rick_and_morty_kobe/lib/pages/details_page.dart
+++ rick_and_morty_kobe/lib/pages/details_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
   static const routeId = '/details';
-  const DetailsPage({super.key});
+  const DetailsPage({super.key, required this.characterId});
+
+  final int characterId;
 
   @override
   Widget build(BuildContext context) {
