efcb788b228cb1e72c8248a8b7566e0e28323e5b
riclovato <riic.lovato@gmail.com>
2025-08-07 17:42:13 -0300

    main routes

--- rick_and_morty_kobe/lib/main.dart
+++ rick_and_morty_kobe/lib/main.dart
@@ -1,43 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty_kobe/widgets/app_bar_widget.dart';
+import 'package:rick_and_morty_kobe/pages/details_page.dart';
+import 'package:rick_and_morty_kobe/pages/home_page.dart';
+
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RickAndMortyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RickAndMortyApp extends StatelessWidget {
+  const RickAndMortyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(),
-      body: Placeholder(),
-     bottomNavigationBar: Container(color: Colors.amber, height: 100,),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case HomePage.routeId:
+            return MaterialPageRoute(
+              settings: settings,
+              builder: (context) => const HomePage(),
+            );
+            case DetailsPage.routeId:
+            return MaterialPageRoute(
+              settings: settings,
+              builder: (context) => const DetailsPage(),
+            );
+          default:
+            return null;
+            
+        }
+      },
     );
   }
 }
---
+++ rick_and_morty_kobe/lib/pages/details_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  static const routeId = '/details';
+  const DetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
